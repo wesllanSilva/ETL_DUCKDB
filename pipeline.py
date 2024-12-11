bcfcef2e29bd42960dbda648a96829d32eb18b15
wesllanSilva <wesllan90@gmail.com>
2024-12-11 17:49:46 -0300

    feat: finalizada funcão transformar e salvar no postgres

--- pipeline.py
+++ pipeline.py
@@ -10,11 +10,12 @@ from pandas import DataFrame
 
 from datetime import datetime
 
-
+load_dotenv()
 
 def baixar_arquivos_google_drive(url_pasta, diretorio_local):
     os.makedirs(diretorio_local, exist_ok=True)
     gdown.download_folder(url_pasta, output=diretorio_local, quiet=False, use_cookies=False)
+
 
 def listar_arquivos_csv(diretorio):
     arquivos_csv = []
@@ -25,9 +26,35 @@ def listar_arquivos_csv(diretorio):
             arquivos_csv.append(caminho_completo)
     return arquivos_csv
 
+# retorna um dataframe_duckdb
+def ler_csv(caminho_arquivo):
+    dataframe_duckdb = duckdb.read_csv(caminho_arquivo)
+    print(dataframe_duckdb)
+    return dataframe_duckdb
+
+#transformacao
+# Função para adicionar uma coluna de total de vendas
+def transformar(df: DuckDBPyRelation) -> DataFrame:
+    # Executa a consulta SQL que inclui a nova coluna, operando sobre a tabela virtual
+    df_transformado = duckdb.sql("SELECT *, quantidade * valor AS total_vendas FROM df").df()
+    # Remove o registro da tabela virtual para limpeza
+    return df_transformado
+
+
+# Função para converter o Duckdb em Pandas e salvar o DataFrame no PostgreSQL
+def salvar_no_postgres(df_duckdb, tabela):
+    DATABASE_URL = os.getenv("DATABASE_URL")  # Ex: 'postgresql://user:password@localhost:5432/database_name'
+    engine = create_engine(DATABASE_URL)
+    # Salvar o DataFrame no PostgreSQL
+    df_duckdb.to_sql(tabela, con=engine, if_exists='append', index=False)
+
 
 if __name__ == "__main__":
     url_pasta = 'https://drive.google.com/drive/folders/1QIrSU88_aAxeVagZ-fiU-8jtKTR0xs3x'
     diretorio_local = './pasta_gdown'
     # baixar_arquivos_google_drive(url_pasta,diretorio_local)
-    listar_arquivos_csv(diretorio_local)
+    lista_de_arquivos = listar_arquivos_csv(diretorio_local)
+    for caminho_do_arquivo in lista_de_arquivos:
+        duck_db_df = ler_csv(caminho_do_arquivo)
+        pandas_df_transformado = transformar(duck_db_df)
+        salvar_no_postgres(pandas_df_transformado, "vendas_calculado")
